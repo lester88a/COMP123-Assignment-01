822a0613f35fb7c143637fac158a2d2f1ad9490e
lester88a <lester88a@gmail.com>
2015-01-28 00:39:15 -0500

    Made some changes for Hero.cs

--- Assignment1/Hero.cs
+++ Assignment1/Hero.cs
@@ -20,12 +20,14 @@ namespace Assignment1
         private int strength;
         private int speed;
         private int health;
+        private int rdmNumber;
+
 
         //creats public properties 
         public string name;
 
         //Constructor methods++++++++++++++++++++++++++++++++++++++++++++++++++++
-        public Hero (string name)
+        public Hero(string name)
         {
             this.name = name;//pass the this.name value to the name properties
 
@@ -33,17 +35,20 @@ namespace Assignment1
         }
 
         // creats the private generateAbilities method
-        private void generateAbilities()
+        public void generateAbilities()
         {
             //generates random numbers of strength, speed ,health between 1 to 100
-            Random strength = new Random();
-            this.strength = strength.Next(1, 100);
+            Random rnm = new Random();
+            this.strength = rnm.Next(1, 100);
+
 
             Random speed = new Random();
-            this.speed = speed.Next(1, 100);
+            this.speed = rnm.Next(1, 100);
+
 
             Random health = new Random();
-            this.health = health.Next(1, 100);
+            this.health = rnm.Next(1, 100);
+
         }
 
         //generates the public fight Method
@@ -53,40 +58,48 @@ namespace Assignment1
             {
                 hitDamage();
             }
-            show();
+
         }
 
         //cerat a private hitAttempt method
         private bool hitAttempt()
         {
-            if (this.health >= 1 && this.health <= 20) //if the hero hits, the health is between 1-20
+
+            Random rdmNumber = new Random();
+            this.rdmNumber = rdmNumber.Next(1, 100);
+
+            if (this.rdmNumber >= 1 && this.rdmNumber <= 20) //if the hero hits, the health will be between 1-20
             {
                 Console.WriteLine("Hero attempt to hit!");
-                Console.WriteLine("The hit attempt score is {0}.", this.health);
                 return true;
             }
-            else
+            else // if not, will return false.
             {
-                Console.WriteLine("Hero is not hit!");
-                Console.WriteLine("generates the this.health false number is {0}.", this.health);
+                Console.WriteLine("Hero did not hit!");
+                Console.WriteLine("generates the this.rdmNumber number which is {0} is more than 20.", this.rdmNumber);
                 return false;
             }
         }
 
         //cerat a private hitDamage method
         private int hitDamage()
-        { 
-            Random randomNumber=new Random();
-            int damages = this.strength * randomNumber.Next(1, 6);// multiply the strength by a random number
+        {
+            Random randomNumber = new Random();//generates a random number.
+            this.rdmNumber = randomNumber.Next(1, 6);
+            int damages = this.strength * this.rdmNumber;// multiply the strength by a random number
+            Console.WriteLine("The hit damage score is {0}, randomNumber{1}.", damages, this.rdmNumber);// display the damage value
             return damages;
-            
+
         }
 
         //display hero's ability score
         public void show()
         {
-
-            Console.WriteLine("The hero's damage score is {0}",this.hitDamage());
+            Console.WriteLine("+++++++++++++++++++++++++++++");
+            Console.WriteLine("--------Strength: {0}--------", this.strength);
+            Console.WriteLine("--------Speed: {0}-----------", this.speed);
+            Console.WriteLine("--------Health: {0}----------", this.health);
+            Console.WriteLine("+++++++++++++++++++++++++++++");
         }
 
 
